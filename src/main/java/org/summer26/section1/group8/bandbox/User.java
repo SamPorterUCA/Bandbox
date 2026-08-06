1e93951156a3bd1c765a1fc458c55a955fe1d02f
MD Raquibul Hasan <rh.nabil27@gmail.com>
2026-08-06 22:12:48 +0600

    added user

---
+++ src/main/java/org/summer26/section1/group8/bandbox/User.java
@@ -0,0 +1,45 @@
+package org.summer26.section1.group8.bandbox;
+
+import javax.management.relation.Role;
+
+public class User {
+    private final String userId;
+    private final String password;
+    private final String name;
+    private final Role role;
+    private final String branch;
+    private final boolean hasOverdueInvoice;
+
+    public User(String userId, String password, String name, Role role, String branch, boolean hasOverdueInvoice) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.role = role;
+        this.branch = branch;
+        this.hasOverdueInvoice = hasOverdueInvoice;
+    }
+
+    public boolean isHasOverdueInvoice() {
+        return hasOverdueInvoice;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+}
