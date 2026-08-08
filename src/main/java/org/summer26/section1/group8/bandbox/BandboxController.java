7766b608a9192172e619cda8bafe02a2491a605c
MD Raquibul Hasan <rh.nabil27@gmail.com>
2026-08-08 21:42:20 +0600

    added fxid and onAction

--- src/main/java/org/summer26/section1/group8/bandbox/BandboxController.java
+++ src/main/java/org/summer26/section1/group8/bandbox/BandboxController.java
@@ -1,4 +1,11 @@
 package org.summer26.section1.group8.bandbox;
 
+import javafx.event.ActionEvent;
+
 public class BandboxController {
+    public void onLogin(ActionEvent actionEvent) {
+    }
+
+    public void onForgetPassword(ActionEvent actionEvent) {
+    }
 }
--- src/main/java/org/summer26/section1/group8/bandbox/CustomerDashboardController.java
+++ src/main/java/org/summer26/section1/group8/bandbox/CustomerDashboardController.java
@@ -1,4 +1,7 @@
 package org.summer26.section1.group8.bandbox;
 
+import javafx.scene.control.TableView;
+
 public class CustomerDashboardController {
+    public TableView upcomingTable;
 }
